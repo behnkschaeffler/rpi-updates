00f5121c8355bc2f56884cee5a9acfcf62ff45f4
Michael Behnke <72397489+behnkschaeffler@users.noreply.github.com>
2021-03-06 13:46:33 +0100

    Create docker_install.sh

---
+++ docker_install.sh
@@ -0,0 +1,5 @@
+# https://docs.docker.com/engine/install/debian/#install-using-the-convenience-script
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker pi
+logout
