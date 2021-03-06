9b2c61bca6963e60d16b866f3bdb8fdf7cda051a
Michael Behnke <72397489+behnkschaeffler@users.noreply.github.com>
2021-03-06 13:46:09 +0100

    Update and rename updates.sh to initial_updates.sh

--- initial_updates.sh
+++ initial_updates.sh
@@ -4,9 +4,3 @@ sudo ntpdate -u ntp.ubuntu.com
 sudo apt-get install ca-certificates
 # https://github.com/Hexxeh/rpi-update
 sudo RPI_REBOOT=1 BRANCH=stable PRUNE_MODULES=1 rpi-update 
-
-# https://docs.docker.com/engine/install/debian/#install-using-the-convenience-script
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo usermod -aG docker pi
-logout
